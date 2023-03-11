e6da7dd848b5352ca95e491883ccb43db68bb2da
Craigory Coppola <craigorycoppola@gmail.com>
2023-03-11 00:31:12 -0500

    chore: ci updates

--- tools/deploy-to-github-pages.ts
+++ tools/deploy-to-github-pages.ts
@@ -1,6 +1,5 @@
 import { execSync, ExecSyncOptions } from 'child_process';
 import { join } from 'path';
-import isCI from 'is-ci';
 
 const cwd = join(__dirname, '../dist/npm-burst');
 const stdio = 'inherit';
@@ -10,23 +9,28 @@ const opts: ExecSyncOptions = {
   stdio,
 };
 
-execSync('git init', opts);
-try {
-  if (isCI) {
-    execSync(
-      `git remote add origin https://github-actions:${process.env.GITHUB_TOKEN}@github.com/agentender/npm-burst`,
-      opts
-    );
-  } else {
-    execSync(
-      'git remote add origin https://github.com/agentender/npm-burst',
-      opts
-    );
+(async () => {
+  execSync('git init', opts);
+  try {
+    if (process.env.CI) {
+      execSync(
+        `git remote add origin https://github-actions:${process.env.GITHUB_TOKEN}@github.com/agentender/npm-burst`,
+        opts
+      );
+    } else {
+      console.log('Using local dev setup');
+      execSync(
+        'git remote add origin https://github.com/agentender/npm-burst',
+        opts
+      );
+    }
+  } catch (e) {
+    console.log();
+    throw e;
+    // its ok
   }
-} catch {
-  // its ok
-}
-execSync('git add .', opts);
-execSync('git commit -m "chore: deploy to gh-pages"', opts);
-execSync('git checkout -B gh-pages', opts);
-execSync('git push -fu origin HEAD', opts);
+  execSync('git add .', opts);
+  execSync('git commit -m "chore: deploy to gh-pages"', opts);
+  execSync('git checkout -B gh-pages', opts);
+  execSync('git push -fu origin HEAD', opts);
+})();
